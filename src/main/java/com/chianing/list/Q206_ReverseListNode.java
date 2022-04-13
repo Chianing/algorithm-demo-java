c4f8c5b821cee2113a27a13fd402075c50898dff
Chianing <chianing9527@gmail.com>
2022-04-13 15:48:20 +0800

    feat: leetCode_206 reverse list node add solution

--- src/main/java/com/chianing/list/Q206_ReverseListNode.java
+++ src/main/java/com/chianing/list/Q206_ReverseListNode.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 
 /**
  * 反转链表
- * https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+ * <a href="https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/">https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/</a>
  */
 public class Q206_ReverseListNode {
 
@@ -40,12 +40,34 @@ public class Q206_ReverseListNode {
     }
 
     /**
-     * 通过栈反转
+     * 双指针反转
      *
      * @param head 头节点
      * @return 反转后的头节点
      */
     public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slowPtr = null, fastPtr = head, tmpPtr;
+        while (fastPtr != null) {
+            tmpPtr = fastPtr.next;
+            fastPtr.next = slowPtr;
+            slowPtr = fastPtr;
+            fastPtr = tmpPtr;
+        }
+
+        return slowPtr;
+    }
+
+    /**
+     * 通过栈反转
+     *
+     * @param head 头节点
+     * @return 反转后的头节点
+     */
+    public ListNode reverseList3(ListNode head) {
         if (head == null) {
             return null;
         }
--- src/test/java/com/chianing/list/Q206_ReverseListNodeTest.java
+++ src/test/java/com/chianing/list/Q206_ReverseListNodeTest.java
@@ -23,7 +23,7 @@ class Q206_ReverseListNodeTest {
     }
 
     @Test
-    void reverseList2() {
+    void testReverseList2() {
         System.out.println("boundary case:");
         LogUtil.printBeforeLog(null);
         LogUtil.printAfterLog(solution.reverseList2(null));
@@ -35,5 +35,20 @@ class Q206_ReverseListNodeTest {
         System.out.println("normal case: ");
         LogUtil.printBeforeLog(node);
         LogUtil.printAfterLog(solution.reverseList2(node));
+    }
+
+    @Test
+    void reverseList2() {
+        System.out.println("boundary case:");
+        LogUtil.printBeforeLog(null);
+        LogUtil.printAfterLog(solution.reverseList3(null));
+
+        System.out.println("single case:");
+        LogUtil.printAfterLog(solution.reverseList3(ListNode.getSingleNode()));
+
+        ListNode node = ListNode.getListNode();
+        System.out.println("normal case: ");
+        LogUtil.printBeforeLog(node);
+        LogUtil.printAfterLog(solution.reverseList3(node));
     }
 }
