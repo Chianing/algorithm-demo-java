9e268ea8118d44f6e8b623c5f3b02edaa60685f1
Chianing <chianing9527@gmail.com>
2022-04-23 15:21:54 +0800

    feat: add shell sort

--- src/main/java/com/chianing/algorithm/Sort.java
+++ src/main/java/com/chianing/algorithm/Sort.java
@@ -60,4 +60,39 @@ public class Sort {
 
     }
 
+    /**
+     * 希尔排序
+     * <p>
+     * 把记录按下标的一定增量分组 对每组使用直接插入排序算法排序
+     * 随着增量逐渐减少 每组包含的关键词越来越多 当增量减至 1 时 整个文件恰被分成一组 算法便终止
+     */
+    public int[] shellSort(int[] nums) {
+        int length = nums.length;
+        if (length <= 1) {
+            return nums;
+        }
+
+        int gap = length / 2;
+        int currentNum, preIndex;
+
+        while (gap != 0) {
+            for (int i = gap; i < length; i++) {
+                currentNum = nums[i];
+                preIndex = i - gap;
+
+                while (preIndex >= 0 && currentNum < nums[preIndex]) {
+                    nums[preIndex + gap] = nums[preIndex];
+                    preIndex -= gap;
+                }
+
+                nums[preIndex + gap] = currentNum;
+            }
+
+            gap /= 2;
+        }
+
+        return nums;
+
+    }
+
 }
--- src/test/java/com/chianing/algorithm/SortTest.java
+++ src/test/java/com/chianing/algorithm/SortTest.java
@@ -8,7 +8,7 @@ class SortTest {
 
     Sort solution = new Sort();
     int[] nums1 = ArrayUtil.getArray(3, 4, 2, 1);
-    int[] nums2 = ArrayUtil.getArray(2, 2, 4, 1, 5);
+    int[] nums2 = ArrayUtil.getArray(8, 9, 1, 7, 2, 3, 5, 4, 6, 0);
 
     @Test
     void bubbleSort() {
@@ -20,5 +20,11 @@ class SortTest {
     void insertSort() {
         LogUtil.printAfterLog(solution.insertSort(nums1));
         LogUtil.printAfterLog(solution.insertSort(nums2));
+    }
+
+    @Test
+    void shellSort() {
+        // LogUtil.printAfterLog(solution.shellSort(nums1));
+        LogUtil.printAfterLog(solution.shellSort(nums2));
     }
 }
